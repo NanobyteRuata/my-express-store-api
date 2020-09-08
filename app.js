e0b24de7cb63483d209a830b384146d555bb5568
Remruat Thanga <remruatthanga@Remruats-MacBook-Pro.local>
2020-09-08 18:54:41 +0630

    commit changes

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const products_route = require('./lib/routes/product_route');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb://localhost/mystore', { useNewUrlParser: true, useUnifiedTopology: true});
+const db = mongoose.connection;
+
+app.use('/api/products', products_route);
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`MyStore app listening at http://localhost:${port}`);
+});
