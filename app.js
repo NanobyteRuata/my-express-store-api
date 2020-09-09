de1b8de308ce23aa91ec9fd5a16cc81d842fc018
Remruat Thanga <remruatthanga@Remruats-MacBook-Pro.local>
2020-09-09 16:32:42 +0630

    commit changes

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const product_route = require('./lib/routes/product_route')
 const category_route = require('./lib/routes/category_route')
+const brand_route = require('./lib/routes/brand_route')
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
@@ -17,6 +18,7 @@ const db = mongoose.connection;
 
 app.use('/api/products', product_route);
 app.use('/api/categories', category_route);
+app.use('/api/brands', brand_route);
 
 app.listen(port, () => {
   console.log(`MyStore app listening at http://localhost:${port}`);
