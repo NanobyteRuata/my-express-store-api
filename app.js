fdcf81749d6081d61d7033509dd6c9452742f025
Remruat Thanga <remruatthanga@Remruats-MacBook-Pro.local>
2020-09-17 20:04:28 +0630

    commit changes

--- app.js
+++ app.js
@@ -1,24 +1,38 @@
 const express = require('express');
-const product_route = require('./lib/routes/product_route')
-const category_route = require('./lib/routes/category_route')
-const brand_route = require('./lib/routes/brand_route')
+const router = require('./lib/routes/router');
 const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+const User = require('./lib/models/user_model');
 const mongoose = require('mongoose');
 
+require("dotenv").config();
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.use(bodyParser.json());
 
-mongoose.connect('mongodb://localhost/mystore', { useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb://localhost/mystore', { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 
-app.use('/api/products', product_route);
-app.use('/api/categories', category_route);
-app.use('/api/brands', brand_route);
+app.use(async (req, res, next) => {
+  if (req.headers["x-access-token"]) {
+    const accessToken = req.headers["x-access-token"];
+    const { userId, exp } = await jwt.verify(accessToken, process.env.JWT_SECRET);
+    // Check if token has expired
+    if (exp < Date.now().valueOf() / 1000) {
+      return res.status(401).json({ error: "JWT token has expired, please login to obtain a new one" });
+    }
+    res.locals.loggedInUser = await User.findById(userId); next();
+  } else {
+    next();
+  }
+});
+
+app.use('/api/', router);
 
 app.listen(port, () => {
   console.log(`MyStore app listening at http://localhost:${port}`);
